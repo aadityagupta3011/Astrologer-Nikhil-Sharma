1357d87ba2aa93bd7429c74e95f4e2f3c0014b2b
Aaditya Gupta <aadityagupta2430@gmail.com>
2024-10-08 23:26:22 +0530

    First Commit

---
+++ astrologerNikhilSharma/astrologer/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Home from './pages/Home';
+import Yantra from './pages/Yantra'; // Ensure the correct path
+import Counseling from './pages/Counseling';
+import Blogs from "./pages/Blogs.jsx"
+import TestiomonailRavindra from './pages/TestiomonailRavindra.jsx';
+import RudrakshaRavindra from './pages/RudrakshaRavindra.jsx';
+import Profile from './pages/Profile.jsx';
+import WhoIsAnkit from './pages/WhoIsAnkit.jsx';
+import Award from './pages/Award.jsx';
+import SimliarPageRavindra from './component/common/SimliarPageRavindra.jsx';
+import AwardsAndAchievements from './component/core/AwardsAndAchievementsRavindra.jsx';
+import AwardsPagesRavindra from './component/core/AwardsPagesRavindra.jsx';
+import AwardsSection from './component/core/AwardsSection.jsx';
+
+
+const App = () => {
+  return (
+    <div>
+      <Blogs />
+    </div>
+  );
+};
+
+export default App;
---
+++ astrologerNikhilSharma/astrologyClient/src/Data/content/LoveHoroscopeContent.jsx
@@ -0,0 +1,175 @@
+const LoveHoroscopeContent = [
+  {
+    heading:
+      "2024 Love Horoscope Forecast for All Zodiac Signs by Astrologer Ankit Sharma Ji",
+    para: [
+      <p className="mb-4">
+        Welcome to the 2024 love horoscope blog! If you are an astrology
+        enthusiast searching for insights into your romantic journey, you've
+        arrived at the right place. This blog delves into the astrological
+        influences on relationships and love for all zodiac signs. Whether
+        you're single and hoping for love or in a relationship seeking to deepen
+        your bond, the stars have messages to guide you. So, sit back and let
+        the cosmic energies lead you toward love, connection, and joy in the
+        year ahead.
+      </p>,
+      <p className="mb-4">
+        To assist people worldwide in making sound, safe, and ideal decisions in
+        matters of love, romance, and relationships in 2024, we offer detailed
+        and precise love horoscope predictions for each of the twelve Zodiac
+        signs. For over a highly successful decade, our renowned astrologer from
+        India has been helping individuals across the globe find clarity and
+        direction in love and relationships through a wide range of services.
+        These highly effective solutions, delivered with speed and at affordable
+        prices, are based on astrology, positive vashikaran, psychic readings,
+        corrective black magic, and hypnotism. Not only love and relationships,
+        but other areas of life are also covered comprehensively by our
+        compassionate astrologer, Ankit Sharma Ji, one of the leading
+        astrologers in India and Asia.
+      </p>,
+      <p className="mb-4">
+        Love and relationships are vital and cherished areas of life, and thus,
+        this field holds special significance for our esteemed astrologer Ankit
+        Sharma Ji. The following 2024 love horoscope predictions for individuals
+        under all zodiac signs have been carefully crafted by our knowledgeable
+        and honorable astrologer to serve you truthfully and sincerely.
+      </p>,
+      <p className="mb-4">
+        The year 2024 is set to bring transformation across various aspects,
+        including love, relationships, health, business, and marriage. The
+        tiger, the symbol of the year, embodies courage and boldness, ensuring
+        that its dynamic energy will influence everyone. Whether it’s finding
+        your true love, discovering new career paths, or achieving greater
+        success in business and wealth, 2024 promises to bring exciting
+        surprises. Astrologer Ankit Sharma Ji offers love problem solutions
+        through astrology and horoscope predictions.
+      </p>,
+      <p className="mb-4">
+        Our expert astrologer, Ankit Sharma Ji, is a highly respected figure who
+        has been delivering love horoscope predictions for many years. His
+        forecasts have helped countless individuals resolve love-related
+        challenges. To enlighten you about your future romantic journey, he is
+        providing valuable 2024 love horoscopes.
+      </p>,
+      <p className="mb-4">
+        Each 2024 love horoscope prediction presented below, tailored for
+        individuals under the twelve Zodiac signs, has been crafted with
+        meticulous attention and deep concern to ensure accuracy and
+        reliability. Ankit Sharma Ji’s love horoscope predictions have amassed a
+        devoted global following over the years. His yearly horoscopes are
+        considered among the most trustworthy and accurate in India and around
+        the world. These predictions are designed to offer valuable insights
+        into potential love challenges and how to prepare in advance to make
+        your love life smoother, safer, and more joyful.
+      </p>,
+    ],
+
+    heading2: "Resolving Love Problems through Kundli Analysis",
+
+    para2: [
+      <p className="mb-4">
+        By carefully analyzing birth charts (Kundli), astrologers can provide
+        powerful and effective solutions to love-related problems. These
+        challenges may arise from personal, mutual, familial, financial, or even
+        astrological issues. This section provides a brief overview of how
+        Kundli analysis can help distressed lovers around the world find
+        solutions.
+      </p>,
+      <p>
+        The following are the key astrological factors in a birth chart that
+        relate to love matters:
+      </p>,
+    ],
+    para3: [
+      <p className="mb-4">
+        These elements are carefully examined in love astrology to devise
+        solutions. The more knowledgeable the astrologer, the better the
+        solutions. Astrologer Ankit Sharma Ji, a globally renowned love
+        astrologer, has helped countless individuals overcome their love
+        struggles over the past three decades. He is also highly acclaimed for
+        his expertise in gemstones and innovative approaches. His unparalleled
+        solutions to love-related problems are available both through personal
+        consultations and online services.
+      </p>,
+    ],
+    ul2: [
+      "The overall conditions of the 7th, 5th, 6th, 8th, 2nd, 11th, 9th, and 12th houses, along with the lords of these houses.",
+      "The positions, tendencies, and strengths of Venus, Jupiter, Mars, Saturn, Rahu, Moon, and Ketu.",
+      "Additional astrological elements, yogas, and planetary combinations.",
+    ],
+
+    asideHeading1: "Love Horoscopes",
+    asideHeading2: "Love Problem Expert",
+    asideHeading3: "Love, Relationships, and Marriage",
+    aside1: [
+      { title: "Aries" },
+      { title: "Taurus" },
+      { title: "Gemini" },
+      { title: "Cancer" },
+      { title: "Leo" },
+      { title: "Virgo" },
+      { title: "Libra" },
+      { title: "Scorpio" },
+      { title: "Sagittarius" },
+      { title: "Aquarius" },
+      { title: "Pisces" },
+      { title: "Most Accurate Love Horoscope" },
+    ],
+
+    aside2: [
+      { title: "Love Problem Solutions" },
+      { title: "Resolving Love Disputes" },
+      { title: "How to Get Your Love Back" },
+      { title: "How to Reconcile with Your Husband After Divorce" },
+      { title: "How to Win Back Your Boyfriend from Another Girl" },
+      { title: "How to Reconcile with Your Ex-Girlfriend" },
+      { title: "How to Reconcile with Your Ex-Boyfriend" },
+      { title: "How to Get Your Ex Back Quickly" },
+      { title: "Intercaste Marriage Love Problems" },
+      { title: "Relationship Solutions" },
+      { title: "Love Marriage Solutions" },
+      { title: "Troubled Marriage Life" },
+      { title: "Intercaste Marriage Issues" },
+      { title: "Extramarital Affairs and Relationships" },
+      { title: "Love Marriage Astrology" },
+      { title: "Love Spells" },
+      { title: "Love Dispute Solutions" },
+      { title: "Intercaste Love Problem Solutions" },
+      { title: "One-Sided Love Solutions" },
+      { title: "Love Triangle Problems" },
+      { title: "Breakup Solutions" },
+    ],
+
+    aside3: [
+      { title: "Troubled Marriage Life" },
+      { title: "Divorce Problem Solutions" },
+      { title: "Best Ways to Prevent Divorce" },
+      { title: "How to Stop Divorce" },
+      { title: "Stopping Divorce through Vashikaran" },
+      { title: "Marriage Astrologer" },
+      { title: "Understanding Problems in Couples" },
+      { title: "Marriage Specialist Astrologer" },
+      { title: "Love Horoscope Specialist" },
+      { title: "Love Astrology by Name and Date of Birth" },
+      { title: "Love Marriage Specialist Astrologer" },
+      { title: "Find a Love Marriage Specialist Near You" },
+      { title: "Best Love Marriage Specialist in India" },
+      { title: "Specialist for Love or Arranged Marriages" },
+      { title: "Win Your Boyfriend Back" },
+      { title: "Win Your Girlfriend Back" },
+      { title: "Love Vashikaran Mantra" },
+      { title: "Reconcile with Your Ex through Vashikaran" },
+      { title: "How to Win Back Lost Love" },
+      { title: "Win Back an Ex-Friend (Boy/Girl)" },
+      { title: "Lost Love Spells" },
+      { title: "Reconcile with Lost Love through Vashikaran" },
+      { title: "Love Psychic Readings" },
+      { title: "Teen Love Problems" },
+      { title: "How to Maintain a Happy and Strong Relationship" },
+      { title: "Characteristics of a Healthy Relationship" },
+      { title: "Vashikaran for Lost Love" },
+    ],
+  },
+];
+
+export default LoveHoroscopeContent;
